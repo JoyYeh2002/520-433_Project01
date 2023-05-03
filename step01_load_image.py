ec727afdb3e37e53eae30eb3f65da5f4033dc980
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-05-03 09:49:53 -0400

    Able to load and display .mhp images

--- step01_load_image.py
+++ step01_load_image.py
@@ -11,16 +11,21 @@ Joy Yeh
 
 import SimpleITK as sitk
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pylab as plt
 
 # Load the .cfg file
 patient_idx = 1
 folder_name = "data/patient" + str(patient_idx).zfill(4) + "/"
-image = sitk.ReadImage(folder_name + 'info_2CH.cfg')
+# image = sitk.ReadImage(folder_name + 'info_4CH.cfg')
 
-# Convert the image to a NumPy array
-image_array = sitk.GetArrayFromImage(image)
+# Load the .mhd file
+I = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + 'patient' + str(patient_idx).zfill(4) + '_2CH_ED.mhd', sitk.sitkFloat32))
 
-# Display the image using matplotlib
-plt.imshow(image_array, cmap='gray')
+# print(ct_scans.shape())
+plt.figure(figsize=(20,16))
+plt.gray()
+plt.subplots_adjust(0,0,1,1,0.01,0.01)
+for i in range(I.shape[0]):
+    plt.subplot(5,6,i+1), plt.imshow(I[i]), plt.axis('off')
+    # use plt.savefig(...) here if you want to save the images as .jpg, e.g.,
 plt.show()
