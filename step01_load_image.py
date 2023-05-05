7fc6f10356a4d383136ce05e21f5234f2a46f3bc
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-05-05 18:30:50 -0400

    Finished the display functionalities.
    Now we can visually inspect all the raw data

--- step01_load_image.py
+++ step01_load_image.py
@@ -13,9 +13,14 @@ import SimpleITK as sitk
 import numpy as np
 import matplotlib.pylab as plt
 
+
 # 0. Basic Control Panel
-patient_idx = 1
+patient_idx = 5
+heartbeat_state = 'ES'  # set to 'ED' or 'ES' to load the corresponding files
 folder_name = "data/patient" + str(patient_idx).zfill(4) + "/"
+display_markings = False
+display_sequence = True
+channel_number = 4
 
 # 1. Open the .cfg file
 cfg_name = 'Info_2CH.cfg'
@@ -40,16 +45,53 @@ print("EDV:", edv)
 print("ESV:", esv)
 print("EF:", ef)
 
-# 2. Load the .mhd file
-mhp_name = 'patient' + str(patient_idx).zfill(4) + '_2CH_ED.mhd'
-I = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_name, sitk.sitkFloat32))
+if display_markings == True:
+    # construct the filenames based on the heartbeat_state
+    mhp_2ch = 'patient{0:04d}_2CH_{1}.mhd'.format(patient_idx, heartbeat_state)
+    mhp_4ch = 'patient{0:04d}_4CH_{1}.mhd'.format(patient_idx, heartbeat_state)
+    mhp_2ch_gt = 'patient{0:04d}_2CH_{1}_gt.mhd'.format(patient_idx, heartbeat_state)
+    mhp_4ch_gt = 'patient{0:04d}_4CH_{1}_gt.mhd'.format(patient_idx, heartbeat_state)
 
-# plt.figure(figsize=(20,16))
-plt.figure()
-plt.gray()
-plt.subplots_adjust(0,0,1,1,0.01,0.01)
-for i in range(I.shape[0]):
-    plt.subplot(5,6,i+1), plt.imshow(I[i]), plt.axis('off')
-    # use plt.savefig(...) here if you want to save the images as .jpg, e.g.,
-plt.show()
+    # load the images and ground truth files
+    I_2ch = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_2ch, sitk.sitkFloat32))
+    I_4ch = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_4ch, sitk.sitkFloat32))
+    I_2ch_gt = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_2ch_gt, sitk.sitkFloat32))
+    I_4ch_gt = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_4ch_gt, sitk.sitkFloat32))
 
+    # create a figure with subplots arranged in a 2x4 grid
+    fig, axs = plt.subplots(2, 4, figsize=(10, 5))
+    plt.gray()
+    plt.subplots_adjust(0, 0, 1, 1, 0.01, 0.01)
+
+    # display each slice of the images side by side in the same subplot
+    for i in range(min(I_2ch.shape[0], 2*4)):
+        axs[i//4, i%4].imshow(I_2ch[i])
+        axs[i//4, i%4].axis('off')
+        axs[i//4, i%4+1].imshow(I_4ch[i])
+        axs[i//4, i%4+1].axis('off')
+        axs[i//4+1, i%4].imshow(I_2ch_gt[i])
+        axs[i//4+1, i%4].axis('off')
+        axs[i//4+1, i%4+1].imshow(I_4ch_gt[i])
+        axs[i//4+1, i%4+1].axis('off')
+
+    # display the figure
+    plt.setp(axs, xticks=[], yticks=[])
+    plt.suptitle('Patient {0}, {1} sequence'.format(patient_idx, heartbeat_state))
+    plt.show()
+
+
+if display_sequence == True:
+    # construct the filename based on the channel_number
+    sequence_file = 'patient{0:04d}_{1}CH_sequence.mhd'.format(patient_idx, channel_number)
+
+    # load the sequence of images
+    I_seq = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + sequence_file, sitk.sitkFloat32))
+
+    # display the images using matplotlib
+    fig, axs = plt.subplots(1, I_seq.shape[0], figsize=(25, 25))
+    for i in range(I_seq.shape[0]):
+        axs[i].imshow(I_seq[i], cmap='gray')
+        axs[i].axis('off')
+
+    plt.suptitle('Patient {0}, {1}CH Sequence'.format(patient_idx, channel_number))
+    plt.show()
