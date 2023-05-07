6097ace6e77def73b181871ba05ac98a8672d432
Hannah <hannahqu08@gmail.com>
2023-05-06 23:00:12 -0400

    gt isolation

--- step04_gt_data_analysis.py
+++ step04_gt_data_analysis.py
@@ -10,6 +10,8 @@ Hannah Qu
 '''
 import SimpleITK as sitk
 import matplotlib.pylab as plt
+import cv2
+import numpy as np
 
 # 0. Basic Control Panel
 patient_idx = 25
@@ -33,17 +35,32 @@ mhp_2ch_gt = 'patient{0:04d}_2CH_{1}_gt.mhd'.format(patient_idx, heartbeat_state
 I = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_2ch, sitk.sitkFloat32))
 I_gt = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_2ch_gt, sitk.sitkFloat32))
 
+
+# Threshold the image to isolate pixels below a certain intensity
+threshold = 0
+low = 0
+high = 255
+binary = []
+for i in range (4):
+    binary.append(I_gt.copy())
+    binary[i][I_gt != i] = 0
+#binary[binary != ] = 255
+#print(np.unique(binary))
+
+
 fig, axs = plt.subplots(1, 4, figsize=(10, 5))
 
-axs[0].imshow(I[0], cmap = 'gray')
-axs[1].imshow(I_gt[0], cmap = 'gray')
-axs[2].imshow(I[0], cmap='gray')
-axs[3].imshow(I_gt[0], cmap = 'gray')
+for i in range(4):
+    axs[i].imshow(binary[i][0], cmap='gray')
+# axs[0].imshow(I[0], cmap = 'gray')
+# axs[1].imshow(I_gt[0], cmap = 'gray')
+# axs[2].imshow(binary[0], cmap='gray')
+# axs[3].imshow(I_gt[0], cmap = 'gray')
 
-axs[0].set_title('P#{0} Orig'.format(patient_idx))
-axs[1].set_title('P#{0} Enhanced'.format(patient_idx))
-axs[2].set_title('P#{0} Orig'.format(patient_idx))
-axs[3].set_title('P#{0} Enhanced'.format(patient_idx))
+axs[0].set_title('P#{0} Binary1'.format(patient_idx))
+axs[1].set_title('P#{0} Binary2'.format(patient_idx))
+axs[2].set_title('P#{0} Binary3'.format(patient_idx))
+axs[3].set_title('P#{0} Binary4'.format(patient_idx))
 
 # remove the ticks from the subplots
 for ax in axs:
