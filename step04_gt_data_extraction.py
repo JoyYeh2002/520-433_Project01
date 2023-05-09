3052205e53fec5e5386ca18f7aa49c54aa383e6b
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-05-09 18:00:33 -0400

    Update data folders, save pickle files, proposed snakes

--- step04_gt_data_extraction.py
+++ step04_gt_data_extraction.py
@@ -4,14 +4,13 @@ Spring 2023
 
 Step04: Get curvature and area information from the gt marking
 - Plot the contour overlayed on the image
-- Save the contour arrays
-- Evolve the snake???
+- Save the contour arrays and processed images to pickle files
 
 Updated 05.09.2023
 
-Hannah Qu, Joy Yeh
+Joy Yeh, Hannah Qu
 '''
-
+import pickle
 import SimpleITK as sitk
 import matplotlib.pylab as plt
 import cv2
@@ -19,12 +18,12 @@ import numpy as np
 from helper_functions import pre_process
 
 # 0. Basic Control Panel
-patient_idx = 5
+patient_idx = 1
 heartbeat_state = 'ES'  # set to 'ED' or 'ES' to load the corresponding files
 folder_name = "data/patient" + str(patient_idx).zfill(4) + "/"
 display_markings = True
 display_sequence = False
-channel_number = 4
+channel_number = 2
 
 # 1. Open the .cfg file
 cfg_name = 'Info_{0}CH.cfg'.format(channel_number)
@@ -56,39 +55,72 @@ r2 = regions[1][0]
 r3 = regions[2][0]
 
 # Get the 3 contours of markings
-c1, _ = cv2.findContours(r1.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-c2, _ = cv2.findContours(r2.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-c3, _ = cv2.findContours(r3.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+c1, _ = cv2.findContours(r1.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # RED
+c2, _ = cv2.findContours(r2.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # GREEN
+c3, _ = cv2.findContours(r3.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # BLUE
 
 ''' Plot on top of original image '''
-# Scale the intensity range of the grayscale image to 0-255
-I = cv2.normalize(I[0], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-I = cv2.cvtColor(I, cv2.COLOR_GRAY2RGB)
+plot_on_orig_img = True
+if plot_on_orig_img == True:
+    # Scale the intensity range of the grayscale image to 0-255
+    I = cv2.normalize(I[0], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    I = cv2.cvtColor(I, cv2.COLOR_GRAY2RGB)
 
-# Set the color of the contour pixels to red in the RGB image
-linewidth = 4
-cv2.drawContours(I, c2, -1, (0, 255, 0), linewidth)
-cv2.drawContours(I, c1, -1, (0, 0, 255), linewidth)
-cv2.drawContours(I, c3, -1, (255, 0, 0), linewidth)
+    # Set the color of the contour pixels to red in the RGB image
+    linewidth = 4
+    cv2.drawContours(I, c2, -1, (0, 255, 0), linewidth)
+    cv2.drawContours(I, c1, -1, (0, 0, 255), linewidth)
+    cv2.drawContours(I, c3, -1, (255, 0, 0), linewidth)
 
-# Save the canvas with the contours in red
-cv2.imwrite('outputs\patinet_{0}_{1}CH_contours_orig.jpg'.format(patient_idx, channel_number), I)
+    # Save the canvas with the contours in red
+    cv2.imwrite('outputs\images\patinet_{0}_{1}CH_contours_orig.jpg'.format(patient_idx, channel_number), I)
 
 ''' Plot on top of processed/enhanced image '''
 I_out = pre_process(np.squeeze(img))
 I_out = cv2.normalize(I_out, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-I_out = cv2.cvtColor(I_out, cv2.COLOR_GRAY2RGB)
 
-# Set the color of the contour pixels to red in the RGB image
-linewidth = 4
-cv2.drawContours(I_out, c2, -1, (0, 255, 0), linewidth)
-cv2.drawContours(I_out, c1, -1, (0, 0, 255), linewidth)
-cv2.drawContours(I_out, c3, -1, (255, 0, 0), linewidth)
+plot_on_img = True
+if plot_on_img == True:
+    I_out = cv2.cvtColor(I_out, cv2.COLOR_GRAY2RGB)
 
-# Save the canvas with the contours in red
-cv2.imwrite('outputs\patinet_{0}_{1}CH_contours_processed.jpg'.format(patient_idx, channel_number), I_out)
+    # Set the color of the contour pixels to red in the RGB image
+    linewidth = 4
+    cv2.drawContours(I_out, c2, -1, (0, 255, 0), linewidth)
+    cv2.drawContours(I_out, c1, -1, (0, 0, 255), linewidth)
+    cv2.drawContours(I_out, c3, -1, (255, 0, 0), linewidth)
 
-# Display the canvas with the contour in red color
-cv2.imshow('Contour Image', I_out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Save the canvas with the contours in red
+    cv2.imwrite('outputs\images\patinet{0:04d}_{1}CH_contours_processed.jpg'.format(patient_idx, channel_number), I_out)
+
+    # Display the canvas with the contour in red color
+    cv2.imshow('Contour Image', I_out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+''' SNAKE data preparation strats here'''
+# Load and pre-process the sequence
+sequence_file = 'patient{0:04d}_{1}CH_sequence.mhd'.format(patient_idx, channel_number)
+I_seq = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + sequence_file, sitk.sitkFloat32))
+
+seq_out = []
+for i in range(I_seq.shape[0]):
+    this_I = I_seq[i]
+    seq_out.append(pre_process(I_seq[i]))
+
+# Save these to a pickle file
+out_file_name = "outputs\pickles\patient{0:04d}_{1}_CH_evolution_variables.pkl".format(patient_idx, channel_number)
+with open(out_file_name, "wb") as f:
+    pickle.dump(patient_idx, f)
+    pickle.dump(channel_number, f)
+    pickle.dump(lines, f)
+
+    pickle.dump(I, f) # Original Image
+    pickle.dump(I_out, f) # Processed img
+    pickle.dump(seq_out, f) # Processed seq
+
+    pickle.dump(c1, f) # Green contour
+    pickle.dump(c2, f) # Red contour
+    pickle.dump(c3, f) # Blue contour
+
+print("Pickle File: patient{0:04d}_{1}_CH_evolution_variables.pkl is saved".format(patient_idx, channel_number))
+
---
+++ step05_snake_evolution.py
@@ -0,0 +1,66 @@
+'''
+EN.520.433 Medical Image Analysis
+Spring 2023
+
+Step04: Get curvature and area information from the gt marking
+- Load the images, initial contours, and 
+
+Updated 05.09.2023
+
+Joy Yeh
+'''
+
+import pickle
+import SimpleITK as sitk
+import matplotlib.pylab as plt
+import cv2
+import numpy as np
+from helper_functions import pre_process
+
+# 0. Load the variables from the pickle file
+patient_idx = 1
+channel_number = 2
+out_file_name = "outputs\pickles\patient{0:04d}_{1}_CH_evolution_variables.pkl".format(patient_idx, channel_number)
+
+with open(out_file_name, "rb") as f:
+    patient_idx = pickle.load(f)
+    channel_number = pickle.load(f)
+    lines = pickle.load(f)
+
+    I = pickle.load(f)
+    I_out = pickle.load(f)
+    I_seq = pickle.load(f)
+
+    c1 = pickle.load(f)
+    c2 = pickle.load(f)
+    c3 = pickle.load(f)
+
+# 1. Plots for sanity check
+plot_on_img = False
+if plot_on_img == True:
+    I_out = cv2.cvtColor(I_out, cv2.COLOR_GRAY2RGB)
+
+    # Set the color of the contour pixels to red in the RGB image
+    linewidth = 4
+    cv2.drawContours(I_out, c2, -1, (0, 255, 0), linewidth)
+    cv2.drawContours(I_out, c1, -1, (0, 0, 255), linewidth)
+    cv2.drawContours(I_out, c3, -1, (255, 0, 0), linewidth)
+
+    # Save the canvas with the contours in red
+    cv2.imwrite('outputs\snakes\patinet{0:04d}_{1}CH_contour_label.jpg'.format(patient_idx, channel_number), I_out)
+
+    # Display the canvas with the contour in red color
+    cv2.imshow('Contour Image', I_out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+plot_seq_samples = True
+if plot_seq_samples == True:
+    fig, axs = plt.subplots(1, int(len(I_seq)/2), figsize=(25, 25))
+    axs[0].imshow(I_out, cmap = 'gray')
+
+    for i in range(1, int(len(I_seq)/2)):
+        axs[i].imshow(I_seq[i], cmap='gray')
+        axs[i].axis('off')
+    
+    plt.show()
