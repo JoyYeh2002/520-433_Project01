1e551e0d1e338500017d9f8df8ad290e9cad6032
Joy Yeh <66350727+JoyYeh2002@users.noreply.github.com>
2023-05-10 14:28:12 -0400

    Add more pickles

--- step04_gt_data_extraction.py
+++ step04_gt_data_extraction.py
@@ -18,111 +18,111 @@ import numpy as np
 from helper_functions import pre_process
 
 # 0. Basic Control Panel
-patient_idx = 10
+patient_idx = 15
 heartbeat_state = 'ES'  # set to 'ED' or 'ES' to load the corresponding files
 folder_name = "data/patient" + str(patient_idx).zfill(4) + "/"
 display_markings = True
 display_sequence = False
-channel_number = 4
 
-# 1. Open the .cfg file
-cfg_name = 'Info_{0}CH.cfg'.format(channel_number)
-with open(folder_name + cfg_name, 'r') as f:
-    # Read the lines of the file into a list
-    lines = f.readlines()
+for channel_number in [2, 4]:
+    # 1. Open the .cfg file
+    cfg_name = 'Info_{0}CH.cfg'.format(channel_number)
+    with open(folder_name + cfg_name, 'r') as f:
+        # Read the lines of the file into a list
+        lines = f.readlines()
 
-# construct the filenames based on the heartbeat_state
-mhp = 'patient{0:04d}_{1}CH_{2}.mhd'.format(patient_idx, channel_number, heartbeat_state)
-mhp_gt = 'patient{0:04d}_{1}CH_{2}_gt.mhd'.format(patient_idx, channel_number, heartbeat_state)
+    # construct the filenames based on the heartbeat_state
+    mhp = 'patient{0:04d}_{1}CH_{2}.mhd'.format(patient_idx, channel_number, heartbeat_state)
+    mhp_gt = 'patient{0:04d}_{1}CH_{2}_gt.mhd'.format(patient_idx, channel_number, heartbeat_state)
 
-# load the images and ground truth files
-I = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp, sitk.sitkFloat32))
-I_gt = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_gt, sitk.sitkFloat32))
+    # load the images and ground truth files
+    I = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp, sitk.sitkFloat32))
+    I_gt = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + mhp_gt, sitk.sitkFloat32))
 
-# Create a memory copy of original image I
-img = I.copy()
+    # Create a memory copy of original image I
+    img = I.copy()
 
-# Threshold the image to isolate pixels below a certain intensity
-regions = []
-for i in range (4):
-    regions.append(I_gt.copy())
-    regions[i][I_gt != i] = 0
-regions = regions[1:]
+    # Threshold the image to isolate pixels below a certain intensity
+    regions = []
+    for i in range (4):
+        regions.append(I_gt.copy())
+        regions[i][I_gt != i] = 0
+    regions = regions[1:]
 
-''' Work with the region'''
-r1 = regions[0][0]
-r2 = regions[1][0]
-r3 = regions[2][0]
+    ''' Work with the region'''
+    r1 = regions[0][0]
+    r2 = regions[1][0]
+    r3 = regions[2][0]
 
-# Get the 3 contours of markings
-c1, _ = cv2.findContours(r1.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # RED
-c2, _ = cv2.findContours(r2.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # GREEN
-c3, _ = cv2.findContours(r3.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # BLUE
+    # Get the 3 contours of markings
+    c1, _ = cv2.findContours(r1.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # RED
+    c2, _ = cv2.findContours(r2.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # GREEN
+    c3, _ = cv2.findContours(r3.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # BLUE
 
-''' Plot on top of original image '''
-plot_on_orig_img = False
-if plot_on_orig_img == True:
-    # Scale the intensity range of the grayscale image to 0-255
-    I = cv2.normalize(I[0], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-    I = cv2.cvtColor(I, cv2.COLOR_GRAY2RGB)
+    ''' Plot on top of original image '''
+    plot_on_orig_img = False
+    if plot_on_orig_img == True:
+        # Scale the intensity range of the grayscale image to 0-255
+        I = cv2.normalize(I[0], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        I = cv2.cvtColor(I, cv2.COLOR_GRAY2RGB)
 
-    # Set the color of the contour pixels to red in the RGB image
-    linewidth = 4
-    cv2.drawContours(I, c2, -1, (0, 255, 0), linewidth)
-    cv2.drawContours(I, c1, -1, (0, 0, 255), linewidth)
-    cv2.drawContours(I, c3, -1, (255, 0, 0), linewidth)
+        # Set the color of the contour pixels to red in the RGB image
+        linewidth = 4
+        cv2.drawContours(I, c2, -1, (0, 255, 0), linewidth)
+        cv2.drawContours(I, c1, -1, (0, 0, 255), linewidth)
+        cv2.drawContours(I, c3, -1, (255, 0, 0), linewidth)
 
-    # Save the canvas with the contours in red
-    cv2.imwrite('outputs\images\patinet_{0}_{1}CH_contours_orig.jpg'.format(patient_idx, channel_number), I)
+        # Save the canvas with the contours in red
+        cv2.imwrite('outputs\images\patinet_{0}_{1}CH_contours_orig.jpg'.format(patient_idx, channel_number), I)
 
-''' Plot on top of processed/enhanced image '''
-I_out = pre_process(np.squeeze(img))
-I_out = cv2.normalize(I_out, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    ''' Plot on top of processed/enhanced image '''
+    I_out = pre_process(np.squeeze(img))
+    I_out = cv2.normalize(I_out, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
-plot_on_img = False
-if plot_on_img == True:
-    I_out = cv2.cvtColor(I_out, cv2.COLOR_GRAY2RGB)
+    plot_on_img = False
+    if plot_on_img == True:
+        I_out = cv2.cvtColor(I_out, cv2.COLOR_GRAY2RGB)
 
-    # Set the color of the contour pixels to red in the RGB image
-    linewidth = 4
-    cv2.drawContours(I_out, c2, -1, (0, 255, 0), linewidth)
-    cv2.drawContours(I_out, c1, -1, (0, 0, 255), linewidth)
-    cv2.drawContours(I_out, c3, -1, (255, 0, 0), linewidth)
+        # Set the color of the contour pixels to red in the RGB image
+        linewidth = 4
+        cv2.drawContours(I_out, c2, -1, (0, 255, 0), linewidth)
+        cv2.drawContours(I_out, c1, -1, (0, 0, 255), linewidth)
+        cv2.drawContours(I_out, c3, -1, (255, 0, 0), linewidth)
 
-    # Save the canvas with the contours in red
-    cv2.imwrite('outputs\images\patinet{0:04d}_{1}CH_contours_processed.jpg'.format(patient_idx, channel_number), I_out)
+        # Save the canvas with the contours in red
+        cv2.imwrite('outputs\images\patinet{0:04d}_{1}CH_contours_processed.jpg'.format(patient_idx, channel_number), I_out)
 
-    # Display the canvas with the contour in red color
-    cv2.imshow('Contour Image', I_out)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        # Display the canvas with the contour in red color
+        cv2.imshow('Contour Image', I_out)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-''' SNAKE data preparation strats here'''
-# Load and pre-process the sequence
-sequence_file = 'patient{0:04d}_{1}CH_sequence.mhd'.format(patient_idx, channel_number)
-I_seq = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + sequence_file, sitk.sitkFloat32))
+    ''' SNAKE data preparation strats here'''
+    # Load and pre-process the sequence
+    sequence_file = 'patient{0:04d}_{1}CH_sequence.mhd'.format(patient_idx, channel_number)
+    I_seq = sitk.GetArrayFromImage(sitk.ReadImage(folder_name + sequence_file, sitk.sitkFloat32))
 
-seq_out = []
-for i in range(I_seq.shape[0]):
-    this_I = I_seq[i]
-    seq_out.append(pre_process(I_seq[i]))
+    seq_out = []
+    for i in range(I_seq.shape[0]):
+        this_I = I_seq[i]
+        seq_out.append(pre_process(I_seq[i]))
 
-# Save these to a pickle file
-out_file_name = "outputs\pickles\patient{0:04d}_{1}_CH_evolution_variables.pkl".format(patient_idx, channel_number)
-with open(out_file_name, "wb") as f:
-    pickle.dump(patient_idx, f)
-    pickle.dump(channel_number, f)
-    pickle.dump(lines, f)
+    # Save these to a pickle file
+    out_file_name = "outputs\pickles\patient{0:04d}_{1}_CH_evolution_variables.pkl".format(patient_idx, channel_number)
+    with open(out_file_name, "wb") as f:
+        pickle.dump(patient_idx, f)
+        pickle.dump(channel_number, f)
+        pickle.dump(lines, f)
 
-    pickle.dump(I, f) # Original Image
-    pickle.dump(I_gt, f) # the label
-    pickle.dump(r2, f) # the binary red contour (the most important one)
-    pickle.dump(I_out, f) # Processed img
-    pickle.dump(seq_out, f) # Processed seq
+        pickle.dump(I, f) # Original Image
+        pickle.dump(I_gt, f) # the label
+        pickle.dump(r2, f) # the binary red contour (the most important one)
+        pickle.dump(I_out, f) # Processed img
+        pickle.dump(seq_out, f) # Processed seq
 
-    pickle.dump(c1, f) # Green contour
-    pickle.dump(c2, f) # Red contour
-    pickle.dump(c3, f) # Blue contour
+        pickle.dump(c1, f) # Green contour
+        pickle.dump(c2, f) # Red contour
+        pickle.dump(c3, f) # Blue contour
 
-print("Pickle File: patient{0:04d}_{1}_CH_evolution_variables.pkl is saved".format(patient_idx, channel_number))
+    print("Pickle File: patient{0:04d}_{1}_CH_evolution_variables.pkl is saved".format(patient_idx, channel_number))
 
--- step05_snake_evolution.py
+++ step05_snake_evolution.py
@@ -16,9 +16,10 @@ import matplotlib.pylab as plt
 import cv2
 import numpy as np
 from helper_functions import pre_process
+import time
 
 # 0. Load the variables from the pickle file
-patient_idx = 20
+patient_idx = 1
 channel_number = 2
 out_file_name = "outputs\pickles\patient{0:04d}_{1}_CH_evolution_variables.pkl".format(patient_idx, channel_number)
 
@@ -28,6 +29,7 @@ with open(out_file_name, "rb") as f:
     lines = pickle.load(f)
 
     I = pickle.load(f)
+    I = I[0]
     I_gt = pickle.load(f)
     r2 = pickle.load(f) # the binary red contour (the most important one)
     
@@ -41,42 +43,50 @@ with open(out_file_name, "rb") as f:
 ''' Method 1: Brute-force segmentation with cartoons '''
 
 # Grab an image without markings 
-idx = 2
-img = I_seq[2]
+for idx in range(1): #(int(len(I_seq)/2)):
+    img = I_seq[idx]
+    img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-# Apply bilateral filtering to create a cartoon-like effect
-filtered = cv2.bilateralFilter(img, 9, 75, 75)
+    # Apply thresholding to create a binary image
+    # _, binary = cv2.threshold(img, 160, 255, cv2.THRESH_BINARY)
 
-# Threshold the filtered image to create a binary image with white, gray, and black colors
-_, thresh = cv2.threshold(filtered, 50, 255, cv2.THRESH_BINARY)
+        # Apply median filtering to remove small details
+    filtered = cv2.medianBlur(img, 25)
 
-# Display the original image and the filtered image side by side
-cv2.imshow('Original Image', img)
-#  cv2.imshow('Filtered Image', filtered)
-cv2.imshow('Thresholded Image', thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    f = filtered.copy()
+    # Identify pixels with values between 30 and 100 and set them to 50
+
+    thresh_low = 30
+    thresh_high = 140
+    f[(f >= thresh_low) & (f <= thresh_high)] = 50
+
+    # Apply adaptive thresholding to create a binary image with gray, black, and white regions
+    block_size = 21
+    c = 2
+    binary = cv2.adaptiveThreshold(filtered, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                    cv2.THRESH_BINARY, block_size, c)
+
+    fig, axs = plt.subplots(1, 4)
+
+    # Plot the images on the subplots
+    axs[0].imshow(I, cmap = 'gray')
+    axs[1].imshow(binary, cmap = 'gray')
+    axs[2].imshow(filtered, cmap = 'gray')
+    axs[3].imshow(f, cmap = 'gray')
+
+    axs[0].set_title('P{0} Img'.format(patient_idx))
+    axs[1].set_title('block = {0}, c = {1}'.format(block_size, c))
+    axs[2].set_title('med blur = 25')
+    axs[3].set_title('thresh = {0}/{1}'.format(thresh_low, thresh_high))
+
+    for ax in axs.flat:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    plt.show()
+
 
 ''' Not Currently Using '''
-# 1. Plot the original marking for sanity check
-plot_on_img = False
-if plot_on_img == True:
-    I_out = cv2.cvtColor(I_out, cv2.COLOR_GRAY2RGB)
-
-    # Set the color of the contour pixels to red in the RGB image
-    linewidth = 4
-    cv2.drawContours(I_out, c2, -1, (0, 255, 0), linewidth)
-    cv2.drawContours(I_out, c1, -1, (0, 0, 255), linewidth)
-    cv2.drawContours(I_out, c3, -1, (255, 0, 0), linewidth)
-
-    # Save the canvas with the contours in red
-    cv2.imwrite('outputs\snakes\patinet{0:04d}_{1}CH_contour_label.jpg'.format(patient_idx, channel_number), I_out)
-
-    # Display the canvas with the contour in red color
-    cv2.imshow('Contour Image', I_out)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
 plot_seq_samples = False
 if plot_seq_samples == True:
     fig, axs = plt.subplots(1, int(len(I_seq)/2), figsize=(25, 25))
